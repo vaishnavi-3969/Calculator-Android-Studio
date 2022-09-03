53b06c8f5def80e6c7758825898b23e7fb08aea9
Vaishnavi Kale <vaishnavi.kale3011@gmail.com>
2022-09-03 21:06:40 +0530

    Updated

--- app/src/main/java/com/example/thebestcalculator/MainActivity.java
+++ app/src/main/java/com/example/thebestcalculator/MainActivity.java
@@ -37,52 +37,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void zeroBTN(View view){
-      updateText("0");
+//      updateText("0");
+        display.append("0");
     }
     public void oneBTN(View view){
-        updateText("1");
+        display.append("1");
     }
     public void twoBTN(View view){
-        updateText("2");
+        display.append("2");
     }
     public void threeBTN(View view){
-        updateText("3");
+        display.append("3");
     }
     public void fourBTN(View view){
-        updateText("4");
+        display.append("4");
     }
     public void fiveBTN(View view){
-        updateText("5");
+        display.append("5");
     }
     public void sixBTN(View view){
-        updateText("6");
+        display.append("6");
     }
     public void sevenBTN(View view){
-        updateText("7");
+        display.append("7");
     }
     public void eightBTN(View view){
-        updateText("8");
+        display.append("8");
     }
     public void nineBTN(View view){
-        updateText("9");
+        display.append("9");
     }
     public void expBTN(View view){
-        updateText("^");
+        display.append("^");
     }
     public void addBTN(View view){
-        updateText("+");
+        display.append("+");
     }
     public void subtractBTN(View view){
-        updateText("-");
+        display.append("-");
     }
     public void clearBTN(View view){
-        updateText("");
+//        updateText("");
+        display.getText().clear();
     }
     public void multiplyBTN(View view){
-        updateText("x");
+        display.append("x");
     }
     public void divideBTN(View view){
-        updateText("/");
+        display.append("÷");
     }
     public void parBTN(View view){
         int cursorPos = display.getSelectionStart();
@@ -90,32 +92,32 @@ public class MainActivity extends AppCompatActivity {
         int closedPar = 0;
         int textLen = display.getText().length();
         for(int i=0; i<cursorPos; i++){
-            if(display.getText().toString().substring(i,i+1).equals("(")){
+            if(display.getText().toString().charAt(i) == '('){
                 openPar +=1;
             }
-            if(display.getText().toString().substring(i,i+1).equals(")")){
+            if(display.getText().toString().charAt(i) == ')'){
                 closedPar +=1;
             }
 
         }
-        if(openPar == closedPar || display.getText().toString().substring(textLen-1,textLen).equals("(")){
+        if(openPar == closedPar || display.getText().toString().charAt(textLen - 1) == '('){
             updateText("(");
             display.setSelection(cursorPos+1);
 
-        }else if(openPar > closedPar && !display.getText().toString().substring(textLen-1,textLen).equals("(")){
+        }else if(openPar > closedPar && display.getText().toString().charAt(textLen - 1) != '('){
             updateText(")");
         }
         display.setSelection(cursorPos+1);
     }
     public void plusMinusBTN(View view){
-        updateText("-");
+        display.append("-");
     }
     public void decimalBTN(View view){
-        updateText(".");
+        display.append(".");
     }
     public void equalBTN(View view){
         String userExp = display.getText().toString();
-        userExp = userExp.replaceAll("/","/");
+        userExp = userExp.replaceAll("÷","/");
         userExp = userExp.replaceAll("x","*");
         Expression exp = new Expression(userExp);
         String result = String.valueOf(exp.calculate());
